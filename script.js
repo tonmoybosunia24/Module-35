27ce85b46e4a00937aee4a4b2574b66ea88e0528
tonmoybosunia24 <mdtonmoybosunia24@gmail.com>
2024-09-16 18:25:26 +0600

    Full Module Complete With Practice

---
+++ script.js
@@ -0,0 +1,53 @@
+// settimeout, setInterval and clearInterval
+
+
+
+// console.log(1)
+// console.log(2)
+// setTimeout(() =>{ /* (setTimeout) এটা ব্যাবহার করা হয় কোন এলিমেন্ট কে পরে দেখানোর জন্য।এখানে সময় টাও সেট করে দেওয়া যায়। */
+//        console.log(3)
+// },2000)
+// console.log(4)
+// let num = 0;
+// const clockId =  setInterval(() =>{ /* (setInterval) কোন একটা কাজকে একটা নিদিষ্ট সময় পরপর ঘটানোর জন্য। */
+//        num++;
+//        if(num > 6){
+//               clearInterval(clockId);
+//        }
+//        console.log(num)
+// }, 2000)
+// console.log(5)
+// console.log(6)
+
+
+
+// JavaScript event loop and concurrency
+
+
+
+// function a(){
+//        console.log("a")
+//        b();
+//        console.log("aa")
+// }
+// function b(){
+//        console.log("b")
+//        c();
+//        console.log("bb")
+// }
+// function c(){
+//        console.log("c")
+//        console.log("cc")
+// }
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+// a();
+
+
+
+
+// Error Handling: try, catch, throw and finally
+
+
+
